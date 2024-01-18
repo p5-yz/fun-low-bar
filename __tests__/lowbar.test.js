caf3aa0dcfc26d3d9a01717c5417f7304f1efea5
AM-Doyle <alexmikedoyle@gmail.com>
2024-01-18 11:09:04 +0000

    Completes challenge 1 of fun-lobar

--- __tests__/lowbar.test.js
+++ __tests__/lowbar.test.js
@@ -1,4 +1,4 @@
-const { identity } = require('../lowbar');
+const { identity, fromPairs } = require('../lowbar');
 
 // don't forget to export and import each of your new functions!
 
@@ -13,5 +13,41 @@ describe('identity', () => {
     expect(identity(arr)).toBe(arr);
     const obj = {};
     expect(identity(obj)).toBe(obj);
+  });
+});
+
+describe('recieve an array of arrays and output a new object containing the key value pairs of the sub arrays', () => {
+  test('Should return an object', () => {
+    // arrange
+    const input = [[]]
+    // act
+    const returnValue = fromPairs(input);
+    // assert
+    expect(typeof returnValue === 'object' && !Array.isArray(returnValue)).toBe(true);
+  });
+  test('Should return an object containing the correct key value pair for a single array', () => {
+    // arrange
+    const input = [['a', 1]];
+    // act
+    const returnValue = fromPairs(input);
+    // assert
+    expect(returnValue).toEqual({a: 1});
+  });
+  test('should return correct output if given two arrays', () => {
+    // arrange
+    const input = [['a', 1], ['b', 2]];
+    // act
+    const returnValue = fromPairs(input);
+    // assert
+    expect(returnValue).toEqual({a: 1, b: 2});
+  });
+  test('Should not mutate input array', () => {
+    // arrange
+    const input = [['a', 1], ['b', 2]];
+    const inputRefrence = [['a', 1], ['b', 2]];
+    // act
+    fromPairs(input);
+    // assert
+    expect(input).toEqual(inputRefrence);
   });
 });
--- lowbar.js
+++ lowbar.js
@@ -2,4 +2,13 @@ function identity(x) {
   return x;
 }
 
-module.exports = { identity };
+function fromPairs(arr) {
+  let output = {};
+  for (let i = 0; i < arr.length; i++){
+    output[arr[i][0]] = arr[i][1];
+  }
+  console.log(output);
+  return output;
+}
+
+module.exports = { identity, fromPairs };
